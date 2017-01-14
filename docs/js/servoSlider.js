05a5f338b26c4f05beaf7cd87ed0202d6310320e
cdra0624 <t.adaniya@adawarp.com>
2017-01-14 14:56:17 +0900

    feat(slideMove):add slide bar

---
+++ docs/js/servoSlider.js
@@ -0,0 +1,9 @@
+function yawVolume(vol) {
+  document.querySelector('#yaw_volume').value = vol;
+  console.log(vol);
+}
+
+function pitchVolume(vol) {
+  document.querySelector('#pitch_volume').value = vol;
+  console.log(vol);
+}
